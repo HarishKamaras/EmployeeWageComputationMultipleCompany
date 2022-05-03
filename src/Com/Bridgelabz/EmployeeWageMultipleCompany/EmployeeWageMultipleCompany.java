d9ef19d90e1535b784210bc4a244e59d550648af
HarishKamaras <harishkamarasu@gmail.com>
2022-05-03 23:43:37 +0530

    MethodForMultipleCompanies

--- src/Com/Bridgelabz/EmployeeWageMultipleCompany/EmployeeWageMultipleCompany.java
+++ src/Com/Bridgelabz/EmployeeWageMultipleCompany/EmployeeWageMultipleCompany.java
@@ -1,16 +1,16 @@
 package Com.Bridgelabz.EmployeeWageMultipleCompany;
 
+import java.util.Scanner;
 
 public class EmployeeWageMultipleCompany {
-    private static final int WAGE_PER_HOUR = 20;
-    private static final int IS_FULL_TIME = 1;
-    private static final int IS_PART_TIME = 2;
     private static final int HALF_DAY_HOUR = 5;
     private static final int FULL_DAY_HOUR = 10;
-    private static final int TOTAL_WORKING_HOURS = 100;
-    private static final int TOTAL_WORKING_DAYS = 20;
-     //UC7 Method to Compute wage
-    public static void wageCompute() {
+
+    private static final int IS_FULL_TIME = 1;
+
+    private static final int IS_PART_TIME = 2;
+
+    public static void wageCompute( String companyName, int WAGE_PER_HOUR, int TOTAL_WORKING_DAYS, int TOTAL_WORKING_HOURS ){
         int dayCount = 1;
         int workingHours = 0;
         int totalWage = 0;
@@ -35,10 +35,23 @@ public class EmployeeWageMultipleCompany {
             System.out.println("Daily Wage for day : " + dayCount + " is " + dailyWage);
             dayCount++;
         }
+        System.out.println("********************************");
+        System.out.println("The Company Name : "+companyName);
+        System.out.println("Total Days : "+ (dayCount-1));
+        System.out.println("Total Working Hours : " + workingHours);
+        System.out.println("Total wage for a month :" +totalWage);
     }
-
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage Computation Problem");
-        wageCompute();
+        //UC8 Employee Wage Computation for multiple companies
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" Enter the Company Name :");
+        String companyName = scan.nextLine();
+        System.out.println("enter the wage per hour: ");
+        int WAGE_PER_HOUR = scan.nextInt();
+        System.out.println("enter the Total Working Days: ");
+        int TOTAL_WORKING_DAYS = scan.nextInt();
+        System.out.println("enter the Total working hour: ");
+        int TOTAL_WORKING_HOURS = scan.nextInt();
+        wageCompute(companyName,WAGE_PER_HOUR,TOTAL_WORKING_DAYS, TOTAL_WORKING_HOURS );
     }
 }
