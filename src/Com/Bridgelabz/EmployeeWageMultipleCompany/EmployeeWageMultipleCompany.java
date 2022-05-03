748211b80809d80002f7bf6dfc03e208e5b2ea57
HarishKamaras <harishkamarasu@gmail.com>
2022-05-03 22:49:55 +0530

    EmployeeWageComputationProblem

---
+++ src/Com/Bridgelabz/EmployeeWageMultipleCompany/EmployeeWageMultipleCompany.java
@@ -0,0 +1,11 @@
+package Com.Bridgelabz.EmployeeWageMultipleCompany;
+
+
+public class EmployeeWageMultipleCompany {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee wage Computation Problem");
+    }
+}
