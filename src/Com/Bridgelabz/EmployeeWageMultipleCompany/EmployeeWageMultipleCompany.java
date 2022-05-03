d012ede30048495b2dbccaa119a603a48cadd870
HarishKamaras <harishkamarasu@gmail.com>
2022-05-03 23:15:07 +0530

    SwitchCase

--- src/Com/Bridgelabz/EmployeeWageMultipleCompany/EmployeeWageMultipleCompany.java
+++ src/Com/Bridgelabz/EmployeeWageMultipleCompany/EmployeeWageMultipleCompany.java
@@ -8,23 +8,23 @@ public class EmployeeWageMultipleCompany {
     private static final int HALF_DAY_HOUR = 5;
     private static final int FULL_DAY_HOUR = 10;
     public static void main(String[] args) {
-
         System.out.println("Welcome to Employee wage Computation Problem");
-        int dailyWage;
+        int dailyWage = 0;
         int empStatus = (int) (Math.random() * 10) % 3;
-        if(empStatus==1) {
-            System.out.println("Employee present");
-            //UC2 calculate Daily wage
-            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("daily wage : "+dailyWage);
-                    }
-        else if (empStatus==2) {
-            System.out.println("Employee present");
-            //UC3 part time calculate Daily wage
-            dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
-            System.out.println("part time daily wage : "+dailyWage);
-        } else{
-                System.out.println("Employee Absent");
-            }
+        //UC4 Switch statement
+        switch (empStatus) {
+            case IS_FULL_TIME:
+                System.out.println("Employee worked FULL TIME");
+                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("dailywage : "+dailyWage);
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee worked PART_TIME");
+                dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+                System.out.println("dailywage : "+dailyWage);
+                break;
+            default:
+                System.out.println("Employee is Absent");
+        }
         }
     }
